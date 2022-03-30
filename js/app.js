5e2c92f70eeb3a8e62c83f34b5b1adcdc39863ab
Tanimismat <tanim.ismat@gmail.com>
2022-03-30 11:13:33 +0600

    item can be removed

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const addToDb = item => {
     // db['alom'] = 1;
     // db[item] = 1;
     const storage = localStorage.getItem('fruits');
+    // not to override the previous data
     if (storage) {
         console.log(typeof storage)
         db = JSON.parse(storage);
@@ -17,4 +18,14 @@ const addToDb = item => {
         db[item] = 1;
     }
     localStorage.setItem('fruits', JSON.stringify(db))
+}
+
+const removeItem = item => {
+    const stored = localStorage.getItem('fruits');
+    if (stored) {
+        const storedObj = JSON.parse(stored);
+        delete storedObj[item];
+        localStorage.setItem('fruits', JSON.stringify(storedObj))
+    }
+
 }
